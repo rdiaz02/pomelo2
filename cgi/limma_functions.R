a475a697ca5bc287ef46ae9ad7d1266a9196db7b
root <root@prot29>
2006-10-20 20:06:15 +0200

    got right the previous change

--- cgi/limma_functions.R
+++ cgi/limma_functions.R
@@ -35,10 +35,9 @@ covariables.model.matrix <- function(class.labels, test.type){
         caughtError("The names of the chosen do not match the names of the additional covariates. Most likely, your add. covars. did not have a first row with names.")
       }
       if(length(Class) != dim(covariables.table[covariable.columns])[1]) {
-      if(length(covariable.columns) < 1) {
         caughtError("Different number of samples in array data and additional covariates. Most likely, your add. covars. did not have a first row with names.")
       }
-
+      
       covariables.matrix         <- cbind(Class, covariables.table[covariable.columns])
       ## make sure we do not get numeric values that are way out there
       numeric.vars <- which(unlist(lapply(covariables.matrix, function(x) is.numeric(x))))
