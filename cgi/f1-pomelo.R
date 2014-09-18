dce62f7c082fdae1351f3fbb948b469e40e3f347
Ramon Diaz-Uriarte (at Lacerta) <rdiaz02@gmail.com>
2014-09-18 18:51:34 +0200

    fixing cox problems

--- cgi/f1-pomelo.R
+++ cgi/f1-pomelo.R
@@ -150,98 +150,261 @@ xdata <- matrix(xdata, nrow = length(num.cols.covariate), byrow = TRUE)
 system("cut -f1 covariate > geneNames")
 geneNames <- read.table("geneNames",quote="", sep = "\t")[, 1]
 
-coxph.fit.pomelo0 <- function (x, y, init = NULL,
-                              control, method = "efron",  rownames = NULL) {
-    warnStatus <- 0
-    naindex <- which(is.na(x))
-    if(length(naindex)) {
-        x <- x[-naindex]
-        y <- y[naindex, ]
-    }
-    x <- as.matrix(x) ## this ain't very efficient
-    n <- nrow(y)
-    if (is.matrix(x)) 
-        nvar <- ncol(x)
-    else if (length(x) == 0) 
-        nvar <- 0
-    else nvar <- 1
-    time <- y[, 1]
-    status <- y[, 2]
-    sorted <- order(time)
-    newstrat <- as.integer(rep(0, n))
-    
-    offset <- rep(0, n)
-    weights <- rep(1, n)
+## directly from coxph.fit with the warnStatus addition and na removal
+## coxph.fit.pomelo0 <- function(x, y, strata, offset, init, control,
+## 			weights, method, rownames)
+##     {
+##         warnStatus <- 0
+##         naindex <- which(is.na(x))
+##         if(length(naindex)) {
+##             x <- x[-naindex]
+##             y <- y[naindex, ]
+##         }
+##     x <- as.matrix(x) ## this ain't very efficient
 
-    stime <- as.double(time[sorted])
-    sstat <- as.integer(status[sorted])
-    if (nvar == 0) {
-        x <- as.matrix(rep(1, n))
-        nullmodel <- TRUE
-        nvar <- 1
-        init <- 0
-        maxiter <- 0
-    }
-    else {
-        nullmodel <- FALSE
-        maxiter <- control$iter.max
-        if (!missing(init) && !is.null(init)) {
-            if (length(init) != nvar) 
-                stop("Wrong length for inital values")
-        }
-        else init <- rep(0, nvar)
-    }
-    coxfit <- .C("coxfit2", iter = as.integer(maxiter), as.integer(n), 
-        as.integer(nvar), stime, sstat, x = x[sorted, ], as.double(offset[sorted] - 
-            mean(offset)), as.double(weights), newstrat, means = double(nvar), 
-        coef = as.double(init), u = double(nvar), imat = double(nvar * 
-            nvar), loglik = double(2), flag = integer(1), double(2 * 
-            n + 2 * nvar * nvar + 3 * nvar), as.double(control$eps), 
-        as.double(control$toler.chol), sctest = as.double(method == 
-            "efron"), PACKAGE = "survival")
-    if (nullmodel) {
-        score <- exp(offset[sorted])
-        coxres <- .C("coxmart", as.integer(n), as.integer(method == 
-            "efron"), stime, sstat, newstrat, as.double(score), 
-            as.double(weights), resid = double(n), PACKAGE = "survival")
-        resid <- double(n)
-        resid[sorted] <- coxres$resid
-        names(resid) <- rownames
-        list(loglik = coxfit$loglik[1], linear.predictors = offset, 
-            residuals = resid, method = c("coxph.null", "coxph"))
-    }
-    else {
-        var <- matrix(coxfit$imat, nvar, nvar)
-        coef <- coxfit$coef
-        if (coxfit$flag < nvar) 
-            which.sing <- diag(var) == 0
-        else which.sing <- rep(FALSE, nvar)
-        infs <- abs(coxfit$u %*% var)
-        if (maxiter > 1) {
-            if (coxfit$flag == 1000) {
-              ## I comment out the warnings: we don't need
-              ## them and they fill up the logs
-##                warning("Ran out of iterations and did not converge")
-                warnStatus <- 2
-            }
-            else {
-                infs <- ((infs > control$eps) & infs > control$toler.inf * 
-                  abs(coef))
-                if (any(infs)) {
-##                  warning(paste("Loglik converged before variable ", 
-##                    paste((1:nvar)[infs], collapse = ","), "; beta may be infinite. "))
-                  warnStatus <- 1
-              }
-            }
-        }
-        names(coef) <- dimnames(x)[[2]]
-        lp <- c(x %*% coef) + offset - sum(coef * coxfit$means)
-        score <- exp(lp[sorted])
-        coef[which.sing] <- NA
-        list(coefficients = coef, var = var, warnStatus = warnStatus,
-             loglik = coxfit$loglik)
-    }
-}
+##     n <-  nrow(y)
+##     if (is.matrix(x)) nvar <- ncol(x)
+##     else {
+## 	if (length(x)==0) nvar <-0
+## 	else nvar <-1
+## 	}
+##     time <- y[,1]
+##     status <- y[,2]
+
+##     # Sort the data (or rather, get a list of sorted indices)
+##     if (length(strata)==0) {
+## 	sorted <- order(time)
+##         strata <- NULL
+## 	newstrat <- as.integer(rep(0,n))
+## 	}
+##     else {
+## 	sorted <- order(strata, time)
+## 	strata <- strata[sorted]
+## 	newstrat <- as.integer(c(1*(diff(as.numeric(strata))!=0), 1))
+## 	}
+##     if (missing(offset) || is.null(offset)) offset <- rep(0,n)
+##     if (missing(weights)|| is.null(weights))weights<- rep(1,n)
+##     else {
+## 	if (any(weights<=0)) stop("Invalid weights, must be >0")
+## 	weights <- weights[sorted]
+## 	}
+##     stime <- as.double(time[sorted])
+##     sstat <- as.integer(status[sorted])
+
+##     if (nvar==0) {
+## 	# A special case: Null model.
+## 	#  (This is why I need the rownames arg- can't use x' names)
+## 	# Set things up for 0 iterations on a dummy variable
+## 	x <- as.matrix(rep(1.0, n))
+## 	nullmodel <- TRUE
+## 	nvar <- 1
+## 	init <- 0
+## 	maxiter <- 0
+## 	}
+##     else {
+## 	nullmodel <- FALSE
+## 	maxiter <- control$iter.max
+## 	if (!missing(init) && length(init)>0) {
+## 	    if (length(init) != nvar) stop("Wrong length for inital values")
+## 	    }
+## 	else init <- rep(0,nvar)
+## 	}
+
+##     storage.mode(weights) <- storage.mode(init) <- "double"
+##     coxfit <- .Call(Ccoxfit6, 
+##                      as.integer(maxiter),
+##                      stime, 
+##                      sstat,
+##                      x[sorted,],
+##                      as.double(offset[sorted]),
+##                      weights,
+##                      newstrat,
+##                      as.integer(method=="efron"),
+##                      as.double(control$eps),
+##                      as.double(control$toler.chol),
+##                      as.vector(init),
+##                      as.integer(1), # internally rescale
+##                     PACKAGE = "survival")  
+
+##     if (nullmodel) {
+## 	score <- exp(offset[sorted])
+## 	coxres <- .C(Ccoxmart, as.integer(n),
+## 				as.integer(method=='efron'),
+## 				stime,
+## 				sstat,
+## 				newstrat,
+## 				as.double(score),
+## 				as.double(weights),
+## 				resid=double(n))
+## 	resid <- double(n)
+## 	resid[sorted] <- coxres$resid
+## 	names(resid) <- rownames
+
+## 	list( loglik = coxfit$loglik[1],
+## 	      linear.predictors = offset,
+## 	      residuals = resid,
+## 	      method= c('coxph.null', 'coxph') )
+## 	}
+##     else {
+## 	var <- matrix(coxfit$imat,nvar,nvar)
+## 	coef <- coxfit$coef
+## 	if (coxfit$flag < nvar) which.sing <- diag(var)==0
+## 	else which.sing <- rep(FALSE,nvar)
+
+## 	infs <- abs(coxfit$u %*% var)
+## 	if (maxiter >1) {
+## 	    if (coxfit$flag == 1000) {
+##                 warnStatus <- 2
+## 		##   warning("Ran out of iterations and did not converge")
+##                }
+## 	    else {
+## 		infs <- ((infs > control$eps) & 
+## 			 infs > control$toler.inf*abs(coef))
+## 		if (any(infs)) {
+##                     warnStatus <- 1
+## 		## warning(paste("Loglik converged before variable ",
+## 		## 	  paste((1:nvar)[infs],collapse=","),
+## 		## 	  "; beta may be infinite. "))
+##             }
+## 		}
+## 	    }
+
+## 	names(coef) <- dimnames(x)[[2]]
+## 	lp <- c(x %*% coef) + offset - sum(coef*coxfit$means)
+## 	score <- exp(lp[sorted])
+## 	coxres <- .C(Ccoxmart, as.integer(n),
+## 				as.integer(method=='efron'),
+## 				stime,
+## 				sstat,
+## 				newstrat,
+## 				as.double(score),
+## 				as.double(weights),
+## 				resid=double(n))
+## 	resid <- double(n)
+## 	resid[sorted] <- coxres$resid
+## 	names(resid) <- rownames
+## 	if (maxiter > 0) coef[which.sing] <- NA  #leave it be if iter=0 is set
+
+##         list(coefficients = coef, var = var, warnStatus = warnStatus,
+##              loglik = coxfit$loglik)
+
+##         ## concordance <- survConcordance.fit(Surv(stime, sstat), lp[sorted],
+##         ##                                    strata, weights)
+
+        
+## 	## list(coefficients  = coef,
+## 	## 	    var    = var,
+## 	## 	    loglik = coxfit$loglik,
+## 	## 	    score  = coxfit$sctest,
+## 	## 	    iter   = coxfit$iter,
+## 	## 	    linear.predictors = as.vector(lp),
+## 	## 	    residuals = resid,
+## 	## 	    means = coxfit$means,
+##         ##             concordance=concordance,
+## 	## 	    method='coxph')
+## 	}
+##    }
+
+
+
+
+
+
+
+
+
+## coxph.fit.pomelo0 <- function (x, y, init = NULL,
+##                               control, method = "efron",  rownames = NULL) {
+##     warnStatus <- 0
+##     naindex <- which(is.na(x))
+##     if(length(naindex)) {
+##         x <- x[-naindex]
+##         y <- y[naindex, ]
+##     }
+##     x <- as.matrix(x) ## this ain't very efficient
+##     n <- nrow(y)
+##     if (is.matrix(x)) 
+##         nvar <- ncol(x)
+##     else if (length(x) == 0) 
+##         nvar <- 0
+##     else nvar <- 1
+##     time <- y[, 1]
+##     status <- y[, 2]
+##     sorted <- order(time)
+##     newstrat <- as.integer(rep(0, n))
+    
+##     offset <- rep(0, n)
+##     weights <- rep(1, n)
+
+##     stime <- as.double(time[sorted])
+##     sstat <- as.integer(status[sorted])
+##     if (nvar == 0) {
+##         x <- as.matrix(rep(1, n))
+##         nullmodel <- TRUE
+##         nvar <- 1
+##         init <- 0
+##         maxiter <- 0
+##     }
+##     else {
+##         nullmodel <- FALSE
+##         maxiter <- control$iter.max
+##         if (!missing(init) && !is.null(init)) {
+##             if (length(init) != nvar) 
+##                 stop("Wrong length for inital values")
+##         }
+##         else init <- rep(0, nvar)
+##     }
+##     coxfit <- .C("coxfit2", iter = as.integer(maxiter), as.integer(n), 
+##         as.integer(nvar), stime, sstat, x = x[sorted, ], as.double(offset[sorted] - 
+##             mean(offset)), as.double(weights), newstrat, means = double(nvar), 
+##         coef = as.double(init), u = double(nvar), imat = double(nvar * 
+##             nvar), loglik = double(2), flag = integer(1), double(2 * 
+##             n + 2 * nvar * nvar + 3 * nvar), as.double(control$eps), 
+##         as.double(control$toler.chol), sctest = as.double(method == 
+##             "efron"), PACKAGE = "survival")
+##     if (nullmodel) {
+##         score <- exp(offset[sorted])
+##         coxres <- .C("coxmart", as.integer(n), as.integer(method == 
+##             "efron"), stime, sstat, newstrat, as.double(score), 
+##             as.double(weights), resid = double(n), PACKAGE = "survival")
+##         resid <- double(n)
+##         resid[sorted] <- coxres$resid
+##         names(resid) <- rownames
+##         list(loglik = coxfit$loglik[1], linear.predictors = offset, 
+##             residuals = resid, method = c("coxph.null", "coxph"))
+##     }
+##     else {
+##         var <- matrix(coxfit$imat, nvar, nvar)
+##         coef <- coxfit$coef
+##         if (coxfit$flag < nvar) 
+##             which.sing <- diag(var) == 0
+##         else which.sing <- rep(FALSE, nvar)
+##         infs <- abs(coxfit$u %*% var)
+##         if (maxiter > 1) {
+##             if (coxfit$flag == 1000) {
+##               ## I comment out the warnings: we don't need
+##               ## them and they fill up the logs
+## ##                warning("Ran out of iterations and did not converge")
+##                 warnStatus <- 2
+##             }
+##             else {
+##                 infs <- ((infs > control$eps) & infs > control$toler.inf * 
+##                   abs(coef))
+##                 if (any(infs)) {
+## ##                  warning(paste("Loglik converged before variable ", 
+## ##                    paste((1:nvar)[infs], collapse = ","), "; beta may be infinite. "))
+##                   warnStatus <- 1
+##               }
+##             }
+##         }
+##         names(coef) <- dimnames(x)[[2]]
+##         lp <- c(x %*% coef) + offset - sum(coef * coxfit$means)
+##         score <- exp(lp[sorted])
+##         coef[which.sing] <- NA
+##         list(coefficients = coef, var = var, warnStatus = warnStatus,
+##              loglik = coxfit$loglik)
+##     }
+## }
 
 
 ## mpi.bcast.Robj2slave(coxph.fit.pomelo0)
@@ -251,15 +414,42 @@ coxph.fit.pomelo0 <- function (x, y, init = NULL,
 ## FIXME
 ## could use something for both forking and cluster
 ## as in ADaCGH2 and the distribute function. Later maybe
+
+options(warn = 2)
+
 cox.parallel <- function(x, time, event, MaxIterationsCox = 200,
                          cores = detectCores()) { 
     res.mat <- matrix(NA, nrow = ncol(x), ncol = 4)
     sobject <- Surv(time,event)
+    options(warn = 2)
     
-    funpap3 <- function (x, sobject, MaxIterationsCox) {
-        out1 <- coxph.fit.pomelo0(x, sobject,
-                                  control = coxph.control(iter.max = MaxIterationsCox))
-        if(out1$warnStatus > 1) {
+    funpap3 <- function (x, y, MaxIterationsCox) {
+        ## naindex <- which(is.na(x))
+        ## if(length(naindex)) {
+        ##     x <- x[-naindex]
+        ##     y <- y[-naindex, ]
+        ## }
+        x <- as.matrix(x)
+        out1 <- try(coxph.fit(x, y,
+                          strata = NULL,
+                          ## weights and offset and init are missing, OK
+                          ## as provision for that in the coxph.fit code
+                          ##
+                          method = "efron",
+                          rownames = NULL,
+                          control = coxph.control(iter.max = MaxIterationsCox)))
+
+        if(inherits(out1, "try-error")) {
+            if(grep("Ran out of iterations", out1)) {
+                warnStatus <- 2
+            } else if(grep("Loglik converged before", out1)) {
+                warnStatus <- 1
+            } else {
+                warnStatus <- 3
+            }
+        }
+        
+        if(out1$warnStatus >= 1) {
             return(c(NA, NA, out1$warnStatus))
         } else {
             sts <- out1$coef/sqrt(out1$var)
